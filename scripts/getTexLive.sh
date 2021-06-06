d81d60158f1e141681af0e8a70d79569285f9b29
Rohit Goswami <rog32@hi.is>
2021-06-06 15:17:56 +0000

    tex: Don't grab everything

--- scripts/getTexLive.sh
+++ scripts/getTexLive.sh
@@ -28,7 +28,7 @@ fi
 # Just including texlua so the cache check above works
 # Needed for any use of texlua even if not testing LuaTeX
 tlmgr install luatex
-tlmgr install scheme-full
+tlmgr install scheme-small
 
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # texlive-latex-base is needed to run pdflatex
